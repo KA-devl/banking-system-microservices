62e66b02ac93dadf5b499aa8939e8fe8545c20bc
KA-devl <anes1999@live.ca>
2023-07-29 16:46:45 -0700

    Changing port numbers

--- employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "DEPARTMENT-SERVICE")
+@FeignClient(url="http://localhost:8080", name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     @GetMapping("api/departments/{departmentCode}")
