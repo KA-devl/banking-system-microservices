4eb99c7a8c977b05f6ba90bf901cc2e67a9137f2
KA-devl <anes1999@live.ca>
2023-07-29 20:31:52 -0700

    Adding API-GATEWAY setup

--- employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -2,8 +2,11 @@ package net.javaguides.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,6 +17,7 @@ public class EmployeeServiceApplication {
 //	public WebClient webClient() {
 //		return WebClient.builder().build();
 //	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
